6422aaf4d6072f4f1fc5b465aa7d73c3db447e65
kabirkhurana12 <24csu296@ncuindia.edu>
2025-02-08 20:33:57 +0530

    q1.cpp
    .cpp

--- asignment/q1.cpp
+++ asignment/q1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+int num;
+  cout<<"kindly enter the num:- ";
+  cin>>num;
+  for (int i=2;i<num;i++){
+     if (num%i==0){
+      cout<<"it is not a  prime num"<<endl;
+      return 0;
+  }
+ }
+cout<< "it is a prime num";       
+
+  return 0;
+}
+
+
 
